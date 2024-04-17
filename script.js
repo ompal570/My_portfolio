f3746b0bd59b48f7263cd543961913186e648dd5
ompal singh <143738234+ompal570@users.noreply.github.com>
2024-04-17 23:27:54 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing animation by ompal singh
+var typed = new Typed(".typing" ,{
+    strings:[ "" ,"Frontend Designer" ,"Frontend developer" , "Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
